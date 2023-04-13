c675a4070f888c42816c3c979a0ed8ce228948c3
dwc04112 <dwc04112@gmail.com>
2023-04-13 15:41:44 +0900

    Fix: 필요없는 console.log 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,20 @@
-import {useRecoilState, useRecoilValue} from "recoil";
-import {toastState} from "@/store/toast";
+
 import useToast from "@/components/hooks/useToast";
 
 
 export default function Home() {
-    const [ toasts, setToast ] = useRecoilState(toastState);
-
     const { addToast } = useToast();
-    const setData =()=> addToast({ content:'initial value'})
-    const clipboard =()=> addToast({ content:'링크가 클립보드에 복사되었습니다.',duration:3000})
-    const getData =()=> addToast({ content:'안녕하세요 Toast 말풍선 입니다',duration:7000, isTop: true})
-    console.log(toasts)
+    const leftBottom =()=> addToast({ content:'initial value'})
+    const clip =()=> addToast({ content:'링크가 클립보드에 복사되었습니다.',duration:3000})
+    const rightTop =()=> addToast({ content:'안녕하세요 Toast 말풍선 입니다',duration:7000, isTop: true})
+
 
     return (
     <div className='top-div'>
         <div>test button</div>
-        <button onClick={setData}>button</button>
-        <button onClick={clipboard}>clip</button>
-        <button onClick={getData}>7s</button>
+        <button onClick={leftBottom}>leftBottom</button>
+        <button onClick={clip}>clip</button>
+        <button onClick={rightTop}>rightTop</button>
     </div>
   )
 }
